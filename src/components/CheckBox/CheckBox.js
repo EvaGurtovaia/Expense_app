071bb736a8166b297054dac9f12b1f3a57cfeb80
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-05-15 19:52:30 -0500

    Change some css style

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const CheckBox = (props) => {
     return (
         <div>
-            <input type="checkbox" value={false} />
+            <input type="checkbox" isChecked={false} key={props.id} />
         </div>
     );
 };
